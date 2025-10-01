e2862db10dd6c1d0b7bb48cc7e884b16f725a38b
Emin Başbayan <emin.basbayan@icloud.com>
2025-10-01 16:05:42 +0300

    ürünler fetch api

--- 04-e-ticaret-projesi/script.js
+++ 04-e-ticaret-projesi/script.js
@@ -1,4 +1,5 @@
 const categoriesGridDOM = document.querySelector('.categories-grid');
+const productsGridDOM = document.querySelector('.products-grid');
 
 const categoryIcons = {
   electronics: 'bi-laptop',
@@ -24,8 +25,55 @@ function displayCategories(categories) {
     .map((category) => {
       const icon = categoryIcons[category];
       return `<div class="category-card">
-      <i class="bi ${icon}"></i>
-    <h3>${category}</h3>
+        <i class="bi ${icon}"></i>
+      <h3>${category}</h3>
+    </div>`;
+    })
+    .join('');
+}
+
+async function fetchProducts() {
+  try {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const products = await res.json();
+
+    displayProducts(products);
+  } catch (error) {
+    console.log(error);
+    alert('Veri yüklenirken hata oldu: ' + error);
+  }
+}
+
+function displayProducts(products) {
+  productsGridDOM.innerHTML = products
+    .map((product) => {
+      const fullStars = Math.floor(product.rating.rate);
+      const hasHalfStar = product.rating.rate % 1 >= 0.5;
+      let stars = '<i class="bi bi-star-fill"></i>'.repeat(fullStars);
+      if (hasHalfStar) stars += '<i class="bi bi-star-half"></i>';
+      const emptyStars = 5 - Math.ceil(product.rating.rate);
+      stars += '<i class="bi bi-star"></i>'.repeat(emptyStars);
+
+      return `<div class="product-card">
+    <img
+      src="${product.image}"
+      alt="${product.title}"
+      class="product-image"
+    />
+    <div class="product-info">
+      <p class="product-category">
+        <i class="bi bi-tag"></i> ${product.category}
+      </p>
+      <h3 class="product-title">${product.title}</h3>
+      <div class="product-rating">
+        <span class="stars">${stars}</span>
+        <span>(${product.rating.count})</span>
+      </div>
+      <p class="product-price">₺${product.price}</p>
+      <button class="add-to-cart">
+        <i class="bi bi-cart-plus"></i> Sepete Ekle
+      </button>
+    </div>
   </div>`;
     })
     .join('');
@@ -33,6 +81,5 @@ function displayCategories(categories) {
 
 document.addEventListener('DOMContentLoaded', () => {
   fetchCategories();
+  fetchProducts();
 });
-
-console.log(categoriesGridDOM);
