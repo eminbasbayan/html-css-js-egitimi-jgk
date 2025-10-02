4e41d2d508fb82b1dca42796b01802a65d54bf3e
Emin Başbayan <emin.basbayan@icloud.com>
2025-10-02 14:59:49 +0300

    sepet quantity arttırma ve azaltma işlemleri

--- 04-e-ticaret-projesi/cart.js
+++ 04-e-ticaret-projesi/cart.js
@@ -7,10 +7,19 @@ let cartItems = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+function emptyCart(cartItems) {
+  if (cartItems.length) {
+    cartContainerDOM.style.display = 'block';
+    emptyCartDOM.style.display = 'none';
+  } else {
+    emptyCartDOM.style.display = 'block';
+    cartContainerDOM.style.display = 'none';
+  }
+}
+
 function displayCartItems(cartItems) {
-  console.log(cartItems);
-  if (cartItems.length) emptyCartDOM.style.display = 'none';
-  cartContainerDOM.style.display = 'block';
+  emptyCart(cartItems);
+
   cartItemsListDOM.innerHTML = cartItems
     .map((cartItem) => {
       const fullStars = Math.floor(cartItem.rating.rate);
@@ -36,6 +45,11 @@ function displayCartItems(cartItems) {
           <span>(${cartItem.rating.count})</span>
         </div>
         <p class="product-price">₺${cartItem.price}</p>
+        <div class="product-quantity-container">
+        <button onclick="updateQuantity(${cartItem.id}, 1)">+</button>
+        <strong class="product-price">${cartItem.quantity}</strong>
+        <button onclick="updateQuantity(${cartItem.id}, 0)">-</button>
+        </div>
         <button class="remove-from-cart" onclick="removeFromCart(${cartItem.id})">
           <i class="bi bi-trash"></i> Sepetten Çıkar
         </button>
@@ -45,6 +59,42 @@ function displayCartItems(cartItems) {
     .join('');
 }
 
+function updateQuantity(cartItemId, value) {
+  const findCartItem = cartItems.find((item) => item.id === cartItemId);
+
+  if (value === 1) {
+    cartItems = cartItems.map((cartItem) => {
+      if (cartItem.id === cartItemId) {
+        return {
+          ...cartItem,
+          quantity: cartItem.quantity + 1,
+        };
+      }
+      return cartItem;
+    });
+  }
+  if (value === 0) {
+    if (findCartItem.quantity === 1) {
+      if (confirm('Ürünü Silme İçin Emin Misin?')) {
+        cartItems = cartItems.filter((item) => item.id !== cartItemId);
+      }
+    } else {
+      cartItems = cartItems.map((cartItem) => {
+        if (cartItem.id === cartItemId) {
+          return {
+            ...cartItem,
+            quantity: cartItem.quantity - 1,
+          };
+        }
+        return cartItem;
+      });
+    }
+  }
+
+  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  displayCartItems(cartItems);
+}
+
 function removeFromCart(cartItemId) {
   cartItems = cartItems.filter((cartItem) => cartItem.id !== cartItemId);
   displayCartItems(cartItems);
@@ -52,6 +102,9 @@ function removeFromCart(cartItemId) {
   localStorage.setItem('cartItems', JSON.stringify(cartItems));
 
   cartCountDOM.textContent = cartItems.length;
+  console.log(cartItems.length);
+
+  emptyCart(cartItems);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
